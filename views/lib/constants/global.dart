590312852b99b0718c497cc392d6485abf3506e8
malachieborohoul <malachieborohoul@gmail.com>
2022-11-21 09:21:09 +0100

    Api and flutter app deployment correcting errors

--- views/lib/constants/global.dart
+++ views/lib/constants/global.dart
@@ -1,4 +1,4 @@
 // String uri = "http://192.168.42.54:3000";
 // String uri = "http://192.168.43.144:3000";
 // String uri = "http://192.168.205.191:3000";
-String uri = "https://taxiscanner.herokuapp.com//";
+String uri = "https://taxiscanner.herokuapp.com";
